492f9913166a968364a51fb2883ba4a5b2c3e767
tech3380722 <tech@deckx.app>
2024-08-29 20:03:42 +0200

    Linked Lists: add and print node

---
+++ typescript/Data structures/Linked Lists/addAndPrintNode.ts
@@ -0,0 +1,56 @@
+// Implement a singly linked list in TypeScript. Include methods for a
+//dding a node to the end of the list and printing all elements.
+
+class ListNode {
+    value: number;
+    next: ListNode | null = null;
+    constructor(value: number) {
+        this.value = value;
+    }
+}
+
+class LinkedList {
+    head: ListNode | null = null;
+
+    addNode(newbie: ListNode) {
+        if (this.head != null && newbie != null){
+            let node = this.head;
+            while (node.next != null){
+                node = node.next;
+            }
+        node.next = newbie;
+        newbie.next = null;
+        }
+    };
+
+    print(){
+        if (this.head != null) {
+            let node = this.head;
+            while (node.next != null){
+                console.log(node.value);
+                node = node.next;
+            }
+            console.log(node.value);
+        }
+    }
+
+    // Implement add and print methods here
+}
+
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+
+const list = new LinkedList();
+list.head = node1;
+list.addNode(node2);
+list.print();
+console.log("\n");
+list.addNode(node3);
+list.print();
+console.log("\n");
+list.addNode(node4);
+list.print();
+
+// run with "npx tsc addAndPrintNode.ts && node addAndPrintNode.js"
